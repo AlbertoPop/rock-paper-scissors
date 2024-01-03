17b3186c3a70579e2eba3594ed1fc777c00d7c47
AlbertoPop <140503542+AlbertoPop@users.noreply.github.com>
2024-01-03 20:25:47 +0200

    Working version with basic ui update

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// We need this function to get a computer choice
 
+const btns = document.querySelectorAll('button');
+const resultdiv = document.querySelector(".updates");
+const roundend = document.querySelector(".round-ended")
+
+const winner = document.createElement('h2');
+const result = document.createElement('p');
+const scorekeep = document.createElement('p');
+const newbtn = document.createElement('button');
+
+// We need this function to get a computer choice
 function getComputerChoice(){
     // We use math.random to get a random number between 1 and 3 on a variable. 
     var randomChoice = Math.floor(Math.random() * 3) + 1;
@@ -13,47 +22,85 @@ function getComputerChoice(){
     }
 }
 
-
-
-// we give score 0 to each player in a variable
-var playerScore = 0;
-var computerScore = 0;
-
-// we play the game until someone is reaching a score of 5. 
-while(playerScore < 5 && computerScore <5){
-    game();
-}
-
+game();
 function game(){
+    var gameover = 0;
+    var playerScore = 0;
+    var computerScore = 0;
+    var userChoice = "";
+
     // The game first requires the user to put his option
-    const userChoice = prompt().toLowerCase();
-    console.log(`You chose ${userChoice}`);
+    btns.forEach(btn =>{
+        btn.addEventListener("click", function(){
+            userChoice = btn.value;
+            console.log(`You chose ${userChoice}`);
+            const computerChoice = getComputerChoice();
+            playRound(userChoice, computerChoice);
+        })
+    })
+
+    
     // The game get the choice of the computer
-    const computerChoice = getComputerChoice();
     // It starts the round
-    playRound(userChoice, computerChoice);
+    
 
     function playRound(userChoice,computerChoice){
         if(userChoice === "rock" && computerChoice === "paper"){
             computerScore++;
-            console.log("You Lose! Paper beats Rock - computer get 1 point");
+            result.textContent = "You Lose! Paper beats Rock - computer get 1 point";
+            resultdiv.appendChild(result);
         } else if(userChoice === "paper" && computerChoice === "scissors"){
-            computerScore++;
-            console.log("You Lose! Scissors beats Paper - computer get 1 point")
+            result.textContent = "You Lose! Scissors beats Paper - computer get 1 point";
+            resultdiv.appendChild(result);
         } else if(userChoice === "scissors" && computerChoice === "rock"){
             computerScore++;
-            console.log("You Lose! Rock beats Scissors - computer get 1 point")
+            result.textContent = "You Lose! Rock beats Scissors - computer get 1 point";
         } else if(userChoice === computerChoice ){
-            console.log("It's a tie")
+            result.textContent = "It's a tie";
+            resultdiv.appendChild(result);
         } else {
             playerScore++;
-            console.log ("You win!! You get 1 point");
+            result.textContent = "You win!! You get 1 point";
+            resultdiv.appendChild(result);
         }
+            scorekeep.textContent = `Player score ${playerScore} vs Computer score ${computerScore}`;
+            result.appendChild(scorekeep);
+
+
+            if(playerScore === 5){
+                winner.textContent = "You won the game!";
+                roundend.appendChild(winner);
+                resultdiv.remove();
+                btns.forEach(btn =>{
+                    btn.remove();
+                });
+                newbtn.textContent = "New Game";
+                roundend.appendChild(newbtn);
+                newbtn.addEventListener('click', function(){
+                    window.location.reload();
+                })
+                gameover = 1;
+            } else if(computerScore === 5){
+                winner.textContent = "YOU LOST THE GAME. PC WON";
+                roundend.appendChild(winner);
+                resultdiv.remove();
+                btns.forEach(btn =>{
+                    btn.remove();
+                });
+                newbtn.textContent = "New Game";
+                roundend.appendChild(newbtn);
+                newbtn.addEventListener('click', function(){
+                    window.location.reload();
+                })
+                gameover = 1;
+        
+            }
     }
 
-    console.log(playerScore);
-    console.log(computerScore);
+    
+
 }
+
 
 
 
